c48f80e70be58ed389d5fe973acd4cac843b156d
Tony Louthan <antlouthan@gmail.com>
2024-06-17 15:41:33 -0400

    added the practice exercise

--- calculateArea/calculate_Area.js
+++ calculateArea/calculate_Area.js
@@ -8,3 +8,16 @@ function calculateArea()
  let area = length * width;
  document.getElementById('result').innerText = `The area of the rectangle is: ${area}`;
 }
+
+let g1, g2, g3;
+
+function calculateCost()
+{
+    g1 = parseFloat(document.getElementById('grocery1').value);
+    g2 = parseFloat(document.getElementById('grocery2').value);
+    g3 = parseFloat(document.getElementById('grocery3').value);
+
+    let total = g1 + g2 + g3;
+
+    document.getElementById('total').innerText = `The total amount is: $${total}.`;
+}
